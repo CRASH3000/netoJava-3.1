528d4a54a2fc4718a81dbc1be4cf8555856f13b7
Vlad <Crash-vd@yandex.ru>
2022-11-22 22:12:24 +0300

    Задание 1: Мили - версия 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiket = 344;
+        int mili = 20;
+        int bonus = tiket / mili;
+
+        System.out.println("Получено миль: " + bonus);
+    }
+}
